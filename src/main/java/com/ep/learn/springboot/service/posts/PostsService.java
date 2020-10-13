3c1d449aff931f17e817bce47589d84a64a0a378
E.Park <epark.grace@gmail.com>
2020-10-13 14:46:07 +0900

    feat: 게시글 목록 조회 기능 API, 뷰 추가

--- src/main/java/com/ep/learn/springboot/service/posts/PostsService.java
+++ src/main/java/com/ep/learn/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.ep.learn.springboot.service.posts;
 
 import com.ep.learn.springboot.domain.posts.Posts;
 import com.ep.learn.springboot.domain.posts.PostsRepository;
+import com.ep.learn.springboot.web.dto.PostsListResponseDto;
 import com.ep.learn.springboot.web.dto.PostsResponseDto;
 import com.ep.learn.springboot.web.dto.PostsSaveRequestDto;
 import com.ep.learn.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,10 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
