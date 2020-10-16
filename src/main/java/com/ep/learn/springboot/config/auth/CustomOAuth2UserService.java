89aeb4a1a2994664d46e273fc4b8fce1bb471295
E.Park <epark.grace@gmail.com>
2020-10-16 15:58:12 +0900

    fix: CustomOAuth2UserService에 누락된 SessionUser import

--- src/main/java/com/ep/learn/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/ep/learn/springboot/config/auth/CustomOAuth2UserService.java
@@ -1,6 +1,7 @@
 package com.ep.learn.springboot.config.auth;
 
 import com.ep.learn.springboot.config.auth.dto.OAuthAttributes;
+import com.ep.learn.springboot.config.auth.dto.SessionUser;
 import com.ep.learn.springboot.domain.user.User;
 import com.ep.learn.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
